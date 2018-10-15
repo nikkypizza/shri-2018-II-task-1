389be9cd4a5806f29d27e864a268ee73c927f39c
Nikita PB <npb.nikita@gmail.com>
2018-10-15 17:42:55 +0300

    Add eslint. Style-fix js + scss files

--- js/page-nav.js
+++ js/page-nav.js
@@ -1,12 +1,12 @@
-(function() {
-  var menuButton = document.querySelector(`.page-header__menu-toggler`);
-  var navList = document.querySelector(`.page-header__nav-list`);
+(function () {
+  const menuButton = document.querySelector(`.page-header__menu-toggler`);
+  const navList = document.querySelector(`.page-header__nav-list`);
 
   // Закрывает меню по умолчанию, меняет img кнопки
   menuButton.style.backgroundImage = `url("img/svg/icon-menu-open.svg")`;
   menuButton.title = `Открыть меню`;
   navList.classList.add(`display-none`);
-  //-------------------------
+  // -------------------------
 
   menuButton.addEventListener(`click`, () => {
     if (menuButton.title === `Открыть меню`) {
@@ -17,17 +17,17 @@
       menuButton.style.backgroundImage = `url("img/svg/icon-menu-closed.svg")`;
       navList.classList.add(`display-none`);
       menuButton.title = `Открыть меню`;
-    };
+    }
   });
 
   if (matchMedia) {
-    var mq = window.matchMedia(`(min-width: 768px)`);
-    mq.addListener(WidthChange);
-    WidthChange(mq);
-  };
+    let mq = window.matchMedia(`(min-width: 768px)`);
+    mq.addListener(widthChange);
+    widthChange(mq);
+  }
 
   // при изменении значения вьюпорта
-  function WidthChange(mq) {
+  function widthChange(mq) {
     if (mq.matches) {
       // при ширине вьюпорта >= 768px
       navList.classList.add(`display-none`);
@@ -39,4 +39,4 @@
       menuButton.style.backgroundImage = `url("img/svg/icon-menu-closed.svg")`;
     }
   }
-})()
+})();
--- js/surveillance.js
+++ js/surveillance.js
@@ -1,17 +1,20 @@
-(function() {
+(function () {
 
   const keyCodes = {
     ESC: 27,
     ENTER: 13
-  }
+  };
   const initialValues = {
     BRIGHTNESS_INITIAL_VALUE: 1,
     CONTRAST_INITIAL_VALUE: 100,
     CLICK_COUNTER_INITIAL: 0
-  }
+  };
   const camerasListItemNodes = document.querySelectorAll(`.cameras__list-item`);
   const videoNodes = document.querySelectorAll(`.cameras__list-item-video`);
   const canvasNodes = document.querySelectorAll(`.video-controls__sound-level`);
+  const camerasHeaderNode = document.querySelector(`.cameras__header`);
+  const CAMERAS_HEADER_NODE_TITLE = camerasHeaderNode.innerHTML;
+
   let videoClickCounter = initialValues.CLICK_COUNTER_INITIAL;
   let brightnessValue = initialValues.BRIGHTNESS_INITIAL_VALUE;
   let contrastValue = initialValues.CONTRAST_INITIAL_VALUE;
@@ -19,7 +22,7 @@
   let audioCtx = window.AudioContext || window.webkitAudioContext;
   let canvas, audioContext, canvasContext, analyser, width, height, dataArray, bufferLength;
 
-  for (el of videoNodes) {
+  for (let el of videoNodes) {
     el.addEventListener(`click`, (evt) => {
       videoClickCounter++;
       if (videoClickCounter > 1) {
@@ -33,17 +36,18 @@
       const videoInputNodes = cameraListItem.querySelectorAll(`.video-controls__input`);
 
       videoControlsNode.style = ``;
+      camerasHeaderNode.textContent = el.title;
 
       // Прячем неактивные видео списка
       cameraListItem.classList.add(`cameras__list-item--active`);
-      for (elem of camerasListItemNodes) {
+      for (let elem of camerasListItemNodes) {
         if (!elem.classList.contains(`cameras__list-item--active`)) {
           elem.classList.add(`visually-hidden`);
-        };
-      };
+        }
+      }
 
       // Добавляем контролы <video>, показываем меню
-      cameraListItem.style.webkitAnimationName = `videoItemOpenAnim`
+      cameraListItem.style.webkitAnimationName = `videoItemOpenAnim`;
       cameraListItem.style.animationPlayState = `running`;
       videoNode.setAttribute(`controls`, ``);
       videoNode.muted = false;
@@ -51,24 +55,24 @@
       videoControlsNode.classList.remove(`visually-hidden`);
       videoControlsNode.style.animationPlayState = `running`;
 
-      //CSS фильтры
-      videoInputNodes.forEach((el) => {
-        const inputId = el.getAttribute(`id`);
+      // CSS фильтры
+      videoInputNodes.forEach((element) => {
+        const inputId = element.getAttribute(`id`);
 
-        el.addEventListener(`input`, () => {
+        element.addEventListener(`input`, () => {
           if (inputId.startsWith(`brightness`)) {
-            brightnessValue = el.value / 100;
-          };
+            brightnessValue = element.value / 100;
+          }
           if (inputId.startsWith(`contrast`)) {
-            contrastValue = el.value;
-          };
+            contrastValue = element.value;
+          }
           videoNode.style.filter = `brightness(${brightnessValue}) contrast(${contrastValue}%)`;
-        })
+        });
       });
 
       // https://developers.google.com/web/updates/2017/09/autoplay-policy-changes#webaudio
       audioContext.resume().then(() => {
-        console.log('Playback resumed successfully');
+        console.log(`Playback resumed successfully`);
       });
 
       const onModalClose = () => {
@@ -77,6 +81,7 @@
         videoClickCounter = initialValues.CLICK_COUNTER_INITIAL;
         brightnessValue = initialValues.BRIGHTNESS_INITIAL_VALUE;
         contrastValue = initialValues.CONTRAST_INITIAL_VALUE;
+        camerasHeaderNode.textContent = CAMERAS_HEADER_NODE_TITLE;
 
         // Убираем контролы <video>, прячем меню
         cameraListItem.style.webkitAnimationName = `videoItemCloseAnim`;
@@ -88,31 +93,31 @@
         videoControlsNode.classList.add(`visually-hidden`);
 
         // Показываем неактивные видео списка
-        for (elem of camerasListItemNodes) {
+        for (let elem of camerasListItemNodes) {
           if (!elem.classList.contains(`cameras__list-item--active`)) {
             elem.classList.remove(`visually-hidden`);
-          };
-        };
+          }
+        }
         cameraListItem.classList.remove(`cameras__list-item--active`);
       };
 
       showAllBtn.addEventListener(`click`, onModalClose);
 
-      window.addEventListener(`keydown`, (evt) => {
-        if (evt.keyCode === keyCodes.ESC) {
+      window.addEventListener(`keydown`, (e) => {
+        if (e.keyCode === keyCodes.ESC) {
           if (document.querySelector(`.cameras__list-item--active`)) {
             onModalClose();
           }
-        };
+        }
       });
     });
 
     el.addEventListener(`keyup`, (evt) => {
       if (evt.keyCode === keyCodes.ENTER) {
         console.log(`Развернуть модельное окно по нажатию на ENTER`);
-      };
-    })
-  };
+      }
+    });
+  }
 
   const initStreams = (videosArr) => {
     const videoUrlTitles = [`cat`, `sosed`, `dog`, `hall`];
@@ -124,17 +129,18 @@
         hls.loadSource(url);
         hls.attachMedia(video);
         video.play();
-      };
+      }
     };
 
-    videosArr.forEach(function(el, index) {
+    videosArr.forEach(function (el, index) {
       initVideo(el, `http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2F${videoUrlTitles[index]}%2Fmaster.m3u8`);
+      el.title = `Камера видеонаблюдения: ${videoUrlTitles[index]}`;
     });
   };
 
   const buildAudioGraph = () => {
-    var mediaElement = videoNodes[0];
-    var sourceNode = audioContext.createMediaElementSource(mediaElement);
+    let mediaElement = videoNodes[0];
+    let sourceNode = audioContext.createMediaElementSource(mediaElement);
 
     analyser = audioContext.createAnalyser();
 
@@ -164,14 +170,14 @@
       canvasContext.fillRect(x, height - barHeight / 2, barWidth, barHeight);
 
       x += barWidth + 1;
-    };
+    }
   };
 
   // --- //
 
-  initStreams(videoNodes);
 
-  window.onload = function() {
+  window.onload = function () {
+    initStreams(videoNodes);
     audioContext = new audioCtx();
 
     canvas = canvasNodes[0];
